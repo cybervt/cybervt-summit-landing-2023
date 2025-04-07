be2379a8338dbd580ac500f408233ddceeeae15b
Q4ZWSX <99418850+Q4ZWSX@users.noreply.github.com>
2025-04-06 22:35:36 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,85 +11,162 @@ import comptiaImg from '../public/img/Comptia-logo.svg.png';
 import triplePointImg from '../public/img/triplepointsecurity.png';
 import percivalImg from '../public/img/percival-logo-new-no-bg.png';
 import northropImg from '../public/img/northropgrumman.png';
+import corpsImg from '../public/img/22_Corps_Shield_FullColor_RGB.png';
 import anteanImg from '../public/img/Antean-Final.png';
 import phantomImg from '../public/img/PhantomImg.png';
 
 const scheduleData = [ 
+
 	{ 
+
 		date: 'April 12th, 2025 (In-person & Virtual)', 
+
 		events: [ 
+
 			{ 
+
 				time: '7-8 AM', 
+
 				activity: 'Reception/Breakfast', 
+
 				details: 'Bagels and coffee are provided to in-person attendees', 
+
 			}, 
+
 			{ 
+
 				time: '8-8:30 AM', 
+
 				activity: 'Welcome / Format Overview', 
+
 				details: '', 
+
 			}, 
+
 			{ 
+
 				time: '8:35-10:00 AM', 
+
 				activity: 'Sponsor Talks', 
+
 				details: 'Hear from our amazing sponsors!', 
+
 			}, 
+
 			{ 
+
 				time: '10:00 AM-5:00 PM', 
+
 				activity: 'Jeopardy Style CTF (Virtual Option Available)', 
+
 				details: 'In Person Rooms Open', 
+
 			}, 
+
 			{ 
+
 				time: '5:00 PM-7:30 AM', 
+
 				activity: 'Jeopardy Style CTF (Virtual Option Available)', 
+
 				details: 'In Person Rooms Closed', 
+
 			}, 
+
 		], 
+
 	}, 
+
 	{ 
+
 		date: 'April 13th, 2025 (In-person)', 
+
 		events: [ 
+
 			{ 
+
 				time: '7:30 AM-10:00 AM ', 
+
 				activity: 'Jeopardy Style CTF (Virtual Option Available)', 
+
 				details: 'In Person Rooms Open ', 
+
 			}, 
+
 			{ 
+
 				time: '7:30am-8:00am', 
+
 				activity: 'Reception/Breakfast', 
+
 				details: 'Bagels, coffee, and donuts provided. Breakfast at hotel recommended, if possible', 
+
 			}, 
+
 			{ 
+
 				time: '8:30am-9:00am', 
+
 				activity: 'Sponsor Talks', 
+
 				details: 'Hear from our amazing sponsors!', 
+
 			}, 
+
 			{ 
+
 				time: '10:00AM-11:30AM', 
+
 				activity: 'Prize Ceremony/Walkthroughs', 
+
 				details: 'Get cool prizes and hear about that challenge you got stuck on (and lunch)', 
+
 			}, 
+
 			{ 
+
 				time: '11:30am-12:00pm', 
+
 				activity: 'Castle Competition Explanation', 
+
 				details: 'Brief on the Purple Team competition (in person only)', 
+
 			}, 
+
 			{ 
+
 				time: '12:00pm-4:30pm', 
+
 				activity: 'Castle Competition', 
+
 				details: 'Purple Team competition (in person only)', 
+
 			}, 
+
 			{ 
+
 				time: '4:30pm-5:00pm', 
+
 				activity: 'Castle Competition Award Ceremony', 
+
 				details: 'Cool prizes (in person only)', 
+
 			}, 
+
 			{ 
+
 				time: '5pm-on', 
+
 				activity: 'Join us for dinner in Blacksburg', 
+
 				details: '', 
+
 			}, 
+
 		], 
+
 	}, 
+
 ]; 
 
 const studentFaqQuestions = [
@@ -141,143 +218,150 @@ const nonStudentFaqQuestions = [
 		title: '$ ./team-size',
 		description: 'Teams will be limited to five people. However, multiple teams from the same university can compete.',
 	},
+
 ];
 
 export default function Index() {
-    return (
-        <div>
-            <Box
-                width='100%'
-                height='100%'
-                maxWidth='lg'
-                p={8}
-                flexGrow={1}
-                sx={{ margin: '0 auto' }}
-            >
-                <Stack spacing={6}>
-                    <Box textAlign='center'>
-                        <Image src={summitImg as StaticImageData} />
-                        <Typography variant='h4'>Welcome to Hacksburg</Typography>
-                        <Typography variant='h4' fontFamily='monospace' style={{ overflowWrap: 'break-word' }}>
-                            <Typewriter
-                                onInit={typewriter => {
-                                    typewriter.typeString('Virginia Tech\'s Annual CTF Competition')
-                                        .pauseFor(3 * 1000 * 60)
-                                        .deleteAll()
-                                        .typeString('Okay, you waited this long. Here\'s a flag for your patience. Submit it during the competition: SummitCTF{1_4m_n0t_Us1ng_L4st_Yrs_flag}')
-                                        .start();
-                                }}
-                            />
-                        </Typography>
-                    </Box>
+	return (
+		<div>
+			<Box
+				width='100%'
+				height='100%'
+				maxWidth='lg'
+				p={8}
+				flexGrow={1}
+				sx={{ margin: '0 auto' }}
+			>
+				<Stack spacing={6}>
+					<Box textAlign='center'>
+						<Image src={summitImg as StaticImageData} />
+						<Typography variant='h4'>Welcome to Hacksburg</Typography>
+						<Typography variant='h4' fontFamily='monospace' style={{ overflowWrap: 'break-word' }}>
+							<Typewriter
+								onInit={typewriter => {
+									typewriter.typeString('Virginia Tech\'s Annual CTF Competition')
+										.pauseFor(3 * 1000 * 60)
+										.deleteAll()
+										/* Good on you for reading the source code, challenger! */
+										.typeString('Okay, you waited this long. Here\'s a flag for your patience. Submit it during the competition: SummitCTF{1_4m_n0t_Us1ng_L4st_Yrs_flag}')
+										.start();
+								}}
+							/>
+						</Typography>
+					</Box>
 
-                    <Stack spacing={2}>
-                        <Typography variant='h5'>Frequently Asked Questions</Typography>
-                        <Divider variant='inset' />
+					<Stack spacing={2}>
+						<Typography variant='h5'>Frequently Asked Questions</Typography>
+						<Divider variant='inset' />
 
-                        <Box>
-                            <Accordion>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls='panel1a-content'
-                                    id='panel1a-header'
-                                >
-                                    <Typography>General Information</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    SummitCTF is an annual Capture the Flag (CTF) competition hosted by the Cybersecurity Club at Virginia Tech (CyberVT). The next competition will have two events, one virtual and one in person.
-                                </AccordionDetails>
+						<Box>
+							<Accordion>
+								<AccordionSummary
+									expandIcon={<ExpandMoreIcon />}
+									aria-controls='panel1a-content'
+									id='panel1a-header'
+								>
+									<Typography>General Information</Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									SummitCTF is an annual Capture the Flag (CTF) competition hosted by the Cybersecurity Club at Virginia Tech (CyberVT). The next competition will have two events, one virtual and one in person.
+								</AccordionDetails>
 
-                                <AccordionDetails>
-                                    The virtual event will be a standard jeopardy style CTF, with some unique more realistic challenges. The in person event will be a attack defense event with competitors fighting each other for control.
-                                </AccordionDetails>
+								<AccordionDetails>
+									The virtual event will be a standard jeopardy style CTF, with some unique more realistic challenges. The in person event will be a attack defense event with competitors fighting each other for control.
+								</AccordionDetails>
 
-                                <AccordionDetails>
-                                    The virtual competition will officially start on {studentCompetitionDateTime.toLocaleDateString()} and last 24 hours.
-                                </AccordionDetails>
-                            </Accordion>
-                            <Accordion>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls='panel1a-content'
-                                    id='panel1a-header'
-                                >
-                                    <Typography>Details For Virtual Event</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    {studentFaqQuestions.map(it => (
-                                        <div key={it.title}>
-                                            <Typography mb={1} variant='h5' fontFamily='monospace'>{it.title}</Typography>
-                                            <Typography mb={2}>{it.description}</Typography>
-                                        </div>
-                                    ))}
-                                </AccordionDetails>
-                            </Accordion>
-                            <Accordion>
-                                <AccordionSummary
-                                    expandIcon={<ExpandMoreIcon />}
-                                    aria-controls='panel1a-content'
-                                    id='panel1a-header'
-                                >
-                                    <Typography>Details For In Person Event</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    {nonStudentFaqQuestions.map(it => (
-                                        <div key={it.title}>
-                                            <Typography mb={1} variant='h5' fontFamily='monospace'>{it.title}</Typography>
-                                            <Typography mb={2}>{it.description}</Typography>
-                                        </div>
-                                    ))}
-                                </AccordionDetails>
-                            </Accordion>
-                        </Box>
-                    </Stack>
+								<AccordionDetails>
+									The virtual competition will officially start on {studentCompetitionDateTime.toLocaleDateString()} and last 24 hours.
+								</AccordionDetails>
+							</Accordion>
+							<Accordion>
+								<AccordionSummary
+									expandIcon={<ExpandMoreIcon />}
+									aria-controls='panel1a-content'
+									id='panel1a-header'
+								>
+									<Typography>Details For Virtual Event</Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									{studentFaqQuestions.map(it => (
+										<div key={it.title}>
+											<Typography mb={1} variant='h5' fontFamily='monospace'>{it.title}</Typography>
+											<Typography mb={2}>{it.description}</Typography>
+										</div>
+									))}
+								</AccordionDetails>
+							</Accordion>
+							<Accordion>
+								<AccordionSummary
+									expandIcon={<ExpandMoreIcon />}
+									aria-controls='panel1a-content'
+									id='panel1a-header'
+								>
+									<Typography>Details For In Person Event</Typography>
+								</AccordionSummary>
+								<AccordionDetails>
+									{nonStudentFaqQuestions.map(it => (
+										<div key={it.title}>
+											<Typography mb={1} variant='h5' fontFamily='monospace'>{it.title}</Typography>
+											<Typography mb={2}>{it.description}</Typography>
+										</div>
+									))}
+								</AccordionDetails>
+							</Accordion>
+						</Box>
+					</Stack>
 
-                    <Stack spacing={2}>
-                        <Typography variant='h5'>Registration</Typography>
-                        <Divider variant='inset' />
-                        <Button variant='outlined' target='_blank' href='https://forms.office.com/Pages/ResponsePage.aspx?id=hGiVYK0Q-kCGPU8yweOjemyRXAedrFJKug2Tcw5CJHFURVMxWDBUMlRQUzZZSVhQSTY0RjY0SzFWRi4u'>Register for Summit 2025</Button>
-                    </Stack>
+					<Stack spacing={2}>
+						<Typography variant='h5'>Registration</Typography>
 
-                    <Stack spacing={2}>
-                        <Typography variant='h5'>Discord Server</Typography>
-                        <Divider variant='inset' />
-                        <Button variant='outlined' target='_blank' href='https://discord.gg/zW9fjpsN7X'>Join us on Discord</Button>
-                    </Stack>
+						<Divider variant='inset' />
+						<Button variant='outlined' target='_blank' href='https://forms.office.com/Pages/ResponsePage.aspx?id=hGiVYK0Q-kCGPU8yweOjemyRXAedrFJKug2Tcw5CJHFURVMxWDBUMlRQUzZZSVhQSTY0RjY0SzFWRi4u'>Register for Summit 2025</Button>
+					</Stack>
 
-                    <Stack spacing={2}>
-                        <Typography variant='h5'>Official Schedule</Typography>
-                        <Divider variant='inset' />
-                        <TableContainer component={Paper}>
-                            <Table>
-                                {scheduleData.map((day, index) => (
-                                    <React.Fragment key={index}>
-                                        <TableHead>
-                                            <TableRow>
-                                                <TableCell colSpan={3}>{day.date}</TableCell>
-                                            </TableRow>
-                                            <TableRow>
-                                                <TableCell>Time</TableCell>
-                                                <TableCell>Activity</TableCell>
-                                                <TableCell>Details</TableCell>
-                                            </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {day.events.map((event, idx) => (
-                                                <TableRow key={idx}>
-                                                    <TableCell>{event.time}</TableCell>
-                                                    <TableCell>{event.activity}</TableCell>
-                                                    <TableCell>{event.details}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                        </TableBody>
-                                    </React.Fragment>
-                                ))}
-                            </Table>
-                        </TableContainer>
-                    </Stack>
+					<Stack spacing={2}>
+						<Typography variant='h5'>Discord Server</Typography>
 
-                    <Stack spacing={2}>
+						<Divider variant='inset' />
+						<Button variant='outlined' target='_blank' href='https://discord.gg/zW9fjpsN7X'>Join us on Discord</Button>
+					</Stack>
+
+					<Stack spacing={2}>
+						<Typography variant='h5'>Official Schedule</Typography>
+
+						<Divider variant='inset' />
+
+						<TableContainer component={Paper}>
+							<Table>
+								{scheduleData.map((day, index) => (
+									<React.Fragment key={index}>
+										<TableHead>
+											<TableRow>
+												<TableCell colSpan={3}>{day.date}</TableCell>
+											</TableRow>
+											<TableRow>
+												<TableCell>Time</TableCell>
+												<TableCell>Activity</TableCell>
+												<TableCell>Details</TableCell>
+											</TableRow>
+										</TableHead>
+										<TableBody>
+											{day.events.map((event, idx) => (
+												<TableRow key={idx}>
+													<TableCell>{event.time}</TableCell>
+													<TableCell>{event.activity}</TableCell>
+													<TableCell>{event.details}</TableCell>
+												</TableRow>
+											))}
+										</TableBody>
+									</React.Fragment>
+								))}
+							</Table>
+						</TableContainer>
+
+					</Stack>
+
+<Stack spacing={2}>
                         <Typography variant='h5'>Sponsors</Typography>
                         <Divider variant='inset' />
                         <Grid container justifyContent='space-evenly' spacing={2}>
@@ -287,8 +371,6 @@ export default function Index() {
                                         <Image src={northropImg as StaticImageData} width='300px' height='100%' objectFit='contain' />
                                     </Box>
                                     <MuiLink href='https://northropgrumman.com/' target='_blank' rel='noreferrer'>
-(training truncated here due to length)
-
                                         <Typography variant='h5' fontWeight='bold' textAlign='center' color='text.primary'>
                                             Northrop Grumman
                                         </Typography>
@@ -305,7 +387,7 @@ export default function Index() {
                                         <Image src={triplePointImg as StaticImageData} width='300px' height='100%' objectFit='contain' />
                                     </Box>
                                     <MuiLink href='https://www.triplepointsecurity.com/' target='_blank' rel='noreferrer'>
-                                        <Typography variant='h5' fontWeight='bold' textawiAlign='center' color='text.primary'>
+                                        <Typography variant='h5' fontWeight='bold' textAlign='center' color='text.primary'>
                                             Triple Point Security
                                         </Typography>
                                     </MuiLink>
@@ -342,7 +424,7 @@ export default function Index() {
                                         </Typography>
                                     </MuiLink>
                                     <Typography textAlign='justify'>
-                                        Silver level sponsor. Percival Engineering provides engineering and cybersecurity solutions for defense and commercial applications.
+                                        Silver level sponsor. Percival Engineering is a people first company focused on cyber, embedded, and critical/operational technology.
                                     </Typography>
                                 </Stack>
                             </Grid>
